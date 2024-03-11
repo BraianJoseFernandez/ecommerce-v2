ed7a5fc26f09899954c75b87cee8420fca730ee1
Braian Jose Fernandez <fernandezbraianjose@gmail.com>
2024-03-11 07:55:41 -0300

    avances en listado de familias

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,5 @@
+@props(['breadcrumbs' => []])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -12,6 +14,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    {{-- font-awesome --}}
+    <script src="https://kit.fontawesome.com/bbe875951d.js" crossorigin="anonymous"></script>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -21,14 +26,25 @@
 
 <body class="font-sans antialiased" x-data="{
     sidebarOpen: false,
+}" :class="{
+    'overflow-y-hidden': sidebarOpen,
 }">
+    <div class="fixed inset-0 bg-gray-900 bg-opacity-50 z-20 sm:hidden" x-show="sidebarOpen" style="display:none;"
+        x-on:click="sidebarOpen = false">
+
+    </div>
+
     @include('layouts.partials.admin.navigation')
     @include('layouts.partials.admin.sidebar')
 
 
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-           {{ $slot }}
+        <div class="mt-14">
+            @include('layouts.partials.admin.breadcrumb')
+            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ">
+
+                {{ $slot }}
+            </div>
         </div>
     </div>
 
